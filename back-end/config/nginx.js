8df080e2e047caf8bb5928bbc71ed53ef96ca11b
kidnes <kidnes88@gmail.com>
2015-06-18 18:23:53 +0800

    修改nginx配置

--- back-end/config/nginx.js
+++ back-end/config/nginx.js
@@ -1,8 +1,8 @@
 module.exports = {
-    logPath : '/usr/local/nginx/logs',
-    backLogPath : '/usr/local/nginx/logs',
+    // logPath : '/usr/local/nginx/logs',
+    // backLogPath : '/usr/local/nginx/logs',
     // logFileName : '1.log'
-    // logPath : '/letv/nginx_log',
-    // backLogPath : '/letv/nginx_log_backup',
+    logPath : '/letv/nginx_log',
+    backLogPath : '/letv/nginx_log_backup',
     logFileName : 'access.log'
 }
